37da0e7d21b385dbe210b709147f433ba06e13cb
Nicholas Boleky <hotfeet100@hotmail.com>
2021-02-05 19:18:58 -0800

    First Commit

---
+++ WeatherDemoApp/View/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherView.swift
+//  WeatherDemoApp
+//
+//  Created by Nicholas Boleky on 2/2/21.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
---
+++ WeatherDemoApp/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemoApp
+//
+//  Created by Nicholas Boleky on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WeatherView()
+        }
+    }
+}
